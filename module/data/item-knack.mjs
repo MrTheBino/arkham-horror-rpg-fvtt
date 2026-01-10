7a21e44599cb7d8926fe695cbcdc260e0790cb3b
MrTheBino <mrthebino@gmail.com>
2026-01-10 07:52:48 +0100

    13.0.13
    - added new dice roll engine by frosnoth
    - added new chat cards by frosnoth
    - improved styled
    - added knacks to NPCs for easier prepping sessions
    - bug fixes

--- module/data/item-knack.mjs
+++ module/data/item-knack.mjs
@@ -7,7 +7,7 @@ export default class ArkhamHorrorKnack extends ArkhamHorrorItemBase {
     const schema = super.defineSchema();
     const requiredInteger = { required: true, nullable: false, integer: true };
     
-    schema.tier = new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 });
+    schema.tier = new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 });
 
     return schema;
   }
